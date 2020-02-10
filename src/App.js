78beb4c3f69b0e08f6455e554593121a6e47587a
ninjafat <davide.mezzetti@hotmail.it>
2020-02-10 11:54:27 +0100

    add calculator style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+function onClick(){
+  
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="inputFormula">
+          900
+        </div>
+        <div id="display" className="output">
+          900
+        </div>
+        <div className="buttons">
+          <button id="" className="bg-dark-red btn-large" onClick={onClick}>AC</button>
+          <button id="" className="bg-grey" onClick={onClick}>/</button>
+          <button id="" className="bg-grey" onClick={onClick}>x</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>7</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>8</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>9</button>
+          <button id="" className="bg-grey" onClick={onClick}>-</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>4</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>5</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>6</button>
+          <button id="" className="bg-grey" onClick={onClick}>+</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>1</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>2</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>3</button>
+          
+          <button id="" className="bg-dark-grey btn-large" onClick={onClick}>0</button>
+          <button id="" className="bg-dark-grey" onClick={onClick}>.</button>
+          <button id="equals" className="bg-dark-blue btn-long" onClick={onClick}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
