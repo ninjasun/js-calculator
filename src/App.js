8e2a109d9b07d8cd63c46aae1b2b298f349fa593
ninjafat <davide.mezzetti@hotmail.it>
2020-02-10 11:59:04 +0100

    solved user stories 1-6

--- src/App.js
+++ src/App.js
@@ -16,23 +16,23 @@ function App() {
           900
         </div>
         <div className="buttons">
-          <button id="" className="bg-dark-red btn-large" onClick={onClick}>AC</button>
-          <button id="" className="bg-grey" onClick={onClick}>/</button>
-          <button id="" className="bg-grey" onClick={onClick}>x</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>7</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>8</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>9</button>
-          <button id="" className="bg-grey" onClick={onClick}>-</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>4</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>5</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>6</button>
-          <button id="" className="bg-grey" onClick={onClick}>+</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>1</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>2</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>3</button>
+          <button id="clear" className="bg-dark-red btn-large" onClick={onClick}>AC</button>
+          <button id="divide" className="bg-grey" onClick={onClick}>/</button>
+          <button id="multiply" className="bg-grey" onClick={onClick}>x</button>
+          <button id="seven" className="bg-dark-grey" onClick={onClick}>7</button>
+          <button id="eight" className="bg-dark-grey" onClick={onClick}>8</button>
+          <button id="nine" className="bg-dark-grey" onClick={onClick}>9</button>
+          <button id="subtract" className="bg-grey" onClick={onClick}>-</button>
+          <button id="four" className="bg-dark-grey" onClick={onClick}>4</button>
+          <button id="five" className="bg-dark-grey" onClick={onClick}>5</button>
+          <button id="six" className="bg-dark-grey" onClick={onClick}>6</button>
+          <button id="add" className="bg-grey" onClick={onClick}>+</button>
+          <button id="one" className="bg-dark-grey" onClick={onClick}>1</button>
+          <button id="two" className="bg-dark-grey" onClick={onClick}>2</button>
+          <button id="three" className="bg-dark-grey" onClick={onClick}>3</button>
           
-          <button id="" className="bg-dark-grey btn-large" onClick={onClick}>0</button>
-          <button id="" className="bg-dark-grey" onClick={onClick}>.</button>
+          <button id="zero" className="bg-dark-grey btn-large" onClick={onClick}>0</button>
+          <button id="decimal" className="bg-dark-grey" onClick={onClick}>.</button>
           <button id="equals" className="bg-dark-blue btn-long" onClick={onClick}>=</button>
         </div>
       </div>
